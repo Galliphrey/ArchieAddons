f970b52f8496ccf0a9994d45b6a5e4469d250413
Galliphrey <buncegaming123@gmail.com>
2025-03-14 17:52:59 +0000

    Began work on New Feature

--- features/jawbusDeathMessageRemover.js
+++ features/jawbusDeathMessageRemover.js
@@ -1,9 +1,9 @@
 import Settings from '../config';
 register('chat', (message, event) => {
     if (Settings.jawbusMessageHider) {
-        if (message && message.toLowerCase().includes("was killed by lord jawbus")) {
+        if (message.toLowerCase().includes("was killed by lord jawbus")) {
             cancel(event);
-            ChatLib.chat("HEHE");
+            ChatLib.chat("HEHE"); // just here as a debug thing atm, will be removed once i get this working
         }
     }
 })
