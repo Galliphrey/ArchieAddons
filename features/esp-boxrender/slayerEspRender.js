02cc0fb1a888717393eec200e6945d13ba2cdf28
Galliphrey <buncegaming123@gmail.com>
2025-03-15 00:18:51 +0000

    First bit of progress, regarding ESP

--- features/esp-boxrender/slayerEspRender.js
+++ features/esp-boxrender/slayerEspRender.js
@@ -1,20 +1,31 @@
 import { EntityArmorStand } from '../../misc/entities';
 import Settings from '../../config';
-// use renderlibv2
-const targetEntity = "Dummy";
+import RenderLibV2 from '../../../RenderLibV2'
 
-register("renderOverlay", () => {
+const targetEntity = "zombie";
+const Color = Java.type("java.awt.Color");
+
+register('renderWorld', () => {
     if (Settings.slayerEspBox) {
-        
+        World.getAllEntitiesOfType(EntityArmorStand).forEach((stand) => {
+            let name = stand.getName().removeFormatting().toLowerCase();
+            if (name.includes(targetEntity) && !name.includes(" 0❤")) {
+              RenderLibV2.drawEspBoxV2(
+                stand.getX(),
+                stand.getY() - 2,
+                stand.getZ(),
+                1,
+                2,
+                1,
+                1,
+                0,
+                0,
+                100,
+                true,
+                3
+              );
+            }
+          });
     };
 });
 console.log('Skaro >>> Slayer ESP Loaded.');
-
-//                    let name = stand.getName().removeFormatting();
-                    // Checks hp name tag to spawned by name tag to check whos boss
-  //                  if (name.includes(`Spawned by: ${Player.getName()}`) && (name.includes('Horror') || name.includes('Packmaster') || name.includes('Broodfather') || name.includes('Seraph') || name.includes('Demonlord') || name.includes('Bloodfiend'))) {
-                        // render esp box
-     //               }
-
-
-     
