56016e782c99aa38b5979b053ad4c4a6212cc6d0
Galliphrey <buncegaming123@gmail.com>
2025-03-15 11:21:56 +0000

    working on  the /esp commands

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Settings from './config';
-import { BOLD, GOLD, LIGHT_PURPLE, RESET, DARK_GRAY, WHITE } from './misc/formatting';
+import { BOLD, GOLD, LIGHT_PURPLE, RESET, DARK_GRAY, WHITE, ITALIC } from './misc/formatting';
 import './features/flareAlert'
 import './features/jawbusDeathMessageRemover'
 import './features/esp-boxrender/jawbusEspRender'
@@ -18,7 +18,18 @@ register("command", (user) => {
 }).setName("testcode");
 
 register("command", (user) => {
-    ChatLib.chat(`***\n${LIGHT_PURPLE}${BOLD}ESP${RESET} Settings\n>>> ${GOLD}Add/Remove${DARK_GRAY} Entities: ${WHITE}bleh${RESET}\n>>> ${GOLD}Change${DARK_GRAY} ESP-Box Colour: ${WHITE}bleh${RESET}\n>>> ${GOLD}Change${DARK_GRAY} ESP-Box Thickness: ${WHITE}bleh${RESET}\n***`)
+    ChatLib.chat(`***\n${LIGHT_PURPLE}${BOLD}ESP${RESET} Settings\n>>> ${GOLD}Add/Remove${DARK_GRAY} Example: ' /esp Add old wolf '${RESET}\n>>> ${GOLD}Colour${DARK_GRAY} Example: ' /esp Colour magenta '\n${DARK_GRAY}${ITALIC}(See a full list of all colours with /espcolours)${RESET}\n>>> ${GOLD}Thickness${DARK_GRAY} Example: This value has no command, please edit it through the main config screen.${RESET}\n***`)
 }).setName("esp");
+
+// make this work :p
+register("command", (user, ...args) => {
+    ChatLib.chat(`Added ${args.join(" ")} to ESP Entities.`)
+}).setName("esp Add");
+
+register("command", (user) => {
+    ChatLib.chat(`***\n${LIGHT_PURPLE}${BOLD}ESP${GOLD} Colours${RESET}\n>>> ${DARK_GRAY}Magenta${RESET}\n>>> ${DARK_GRAY}Red${RESET}\n>>> ${DARK_GRAY}Cyan${RESET}\n>>> ${DARK_GRAY}Blue${RESET}\n>>> ${DARK_GRAY}Green${RESET}\n>>> ${DARK_GRAY}Yellow${RESET}\n>>> ${DARK_GRAY}Orange${RESET}\n>>> ${DARK_GRAY}White${RESET}\n>>> ${DARK_GRAY}Black${RESET}\n***`)
+}).setName("espcolours");
+
+
 
 // the amount of dashes to span all of chat: ----------------------------------------------------
