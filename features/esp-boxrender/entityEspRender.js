a5dd5357e0310bddb928c1e8d7e5eb0ad96c53ec
Galliphrey <buncegaming123@gmail.com>
2025-03-15 18:29:54 +0000

    added some variation to the h/w/d

--- features/esp-boxrender/entityEspRender.js
+++ features/esp-boxrender/entityEspRender.js
@@ -17,6 +17,11 @@ const colours = [
     RenderLibV2.getColor(Color.BLACK)
 ]
 
+// have these set to certain values based on the entity
+let width = 0;
+let height = 0;
+let depth = 0;
+
 register('renderWorld', () => {
     if (Settings.entityEspBox) {
       let index = Settings.espColour;
@@ -24,13 +29,40 @@ register('renderWorld', () => {
       World.getAllEntitiesOfType(EntityArmorStand).forEach((stand) => {
           let name = stand.getName().removeFormatting().toLowerCase();
             if (targetEntities.some(entity => name.includes(entity)) && !name.includes(" 0❤")) {
+              if (name.includes("dummy" || "lord jawbus")) {
+                // iron golem entity
+                width = 2;
+                height = 3;
+                depth = 1;
+              } else if (name.includes("old wolf" || "wolf")) {
+                // dog entity
+                width = 2;
+                height = 1;
+                depth = 1;
+              } else if (name.includes("milenia-aged blaze")) {
+                // blaze entity
+                width = 1;
+                height = 1;
+                depth = 1;
+              } else if (name.includes("fels")) {
+                // enderman entity
+                width = 1;
+                height = 3;
+                depth = 1;
+              } else {
+                width = 1;
+                height = 2;
+                depth = 1;
+              }
+
+
               RenderLibV2.drawEspBoxV2(
                 stand.getX(),
-                stand.getY() - 3,
+                stand.getY() - height,
                 stand.getZ(),
-                2,
-                3,
-                1,
+                width,
+                height,
+                depth,
                 renderColor.red,
                 renderColor.green,
                 renderColor.blue,
