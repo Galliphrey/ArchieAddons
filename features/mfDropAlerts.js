f9b3f757f6495f4a9a2c832fed41d8800b4b6bf4
Galliphrey <buncegaming123@gmail.com>
2025-03-16 01:02:23 +0000

    began work on vial drop title

--- features/mfDropAlerts.js
+++ features/mfDropAlerts.js
@@ -5,7 +5,7 @@ import '../misc/formatting';
 register('chat', (message) => {
     if (Settings.mfdropAlertVIAL) {
         if (message.includes('You have received a Mythic Forge Drop!')) {
-            Client.showTitle('Mythic Forge Drop!', 'You have received a Mythic Forge Drop!', 0, 40, 10);
+            Client.showTitle('Radioactive Vial', `+number% magic find`, 5, 60, 15);
         }
     }
 }).setCriteria('You have received a Mythic Forge Drop!');
